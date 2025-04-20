68a2bb6bf7a8e8915b458a043ac1762a695309e3
Jbrooks-UAB <cole.brooks0102@gmail.com>
2025-04-19 22:11:35 -0500

    Inshallah this will be the last commit I need to do tonight

---
+++ Tello-SDK/src/main/java/tello/movementMap.java
@@ -0,0 +1,72 @@
+package tello;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class movementMap extends JPanel {
+
+    private static final int sizeX = 500;
+    private static final int sizeY = 500;
+
+    private int x = sizeX / 2;
+    private int y = sizeY / 2;
+    private int movex = x;
+    private int movey = y;
+
+    private Timer animationtimer;
+
+    public movementMap() {
+        setPreferredSize(new Dimension(sizeX, sizeY));
+        animationtimer = new Timer(30, e -> stepanimation());
+        animationtimer.start();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.RED);
+        g.fillOval(x - 5, y - 5, 10, 10);
+        //System.out.println("building map");
+    }
+
+    private void stepanimation() {
+        int speed = 2;
+        boolean move = false;
+
+        if (x < movex) {
+            x = Math.min(x + speed, movex);
+            move = true;
+        } else if (x > movex) {
+            x = Math.max(x - speed, movex);
+            move = true;
+        }
+
+        if (y < movey) {
+            y = Math.min(y + speed, movey);
+            move = true;
+        } else if (y > movey) {
+            y = Math.max(y - speed, movey);
+            move = true;
+        }
+
+        if (move) {
+            repaint();
+        }
+    }
+
+    public void moveUp() {
+        movey = Math.max(0, movey - 20);
+    }
+
+    public void moveDown() {
+        movey = Math.min(sizeY, movey + 20);
+    }
+
+    public void moveLeft() {
+        movex = Math.max(0, movex - 20);
+    }
+
+    public void moveRight() {
+        movex = Math.min(sizeX, movex + 20);
+    }
+}
--- Tello-SDK/src/main/java/tello/myDemo.java
+++ Tello-SDK/src/main/java/tello/myDemo.java
@@ -4,12 +4,14 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,24 +20,27 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+// These are for socket video streaming to process the images
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+
 import tellolib.communication.TelloConnection;
 import tellolib.control.TelloControl;
 import tellolib.drone.TelloDrone;
 import tellolib.camera.TelloCamera;
-import tellolib.camera.FaceDetection;
 import tellolib.command.TelloFlip;
 
-public class myDemo extends JFrame 
-{
+public class myDemo extends JFrame {
 
     private static final long serialVersionUID = 1L;
     private JFrame frame;
     private final Logger logger = Logger.getGlobal();
-    private TelloCamera	camera;
-	
-	
-    public void execute() 
-	{
+    private TelloCamera camera;
+    private JFrame mapFrame; // <-the map in question
+    private movementMap mapPanel;
+
+    public void execute() {
         //KILLING MYSELF
         frame = new JFrame("Pilot Controls");
         frame.setSize(600, 400);
@@ -51,74 +56,101 @@ public class myDemo extends JFrame
         JButton left = new JButton("Left");
         JButton picture = new JButton("Picture");
 
-        //JPANEL GAPS AND SHIT
-        JPanel mainPanel = new JPanel(new BorderLayout(10, 10));
+         //when the map uh... the uhhhh...
+         mapPanel = new movementMap();
+         mapFrame = new JFrame("Drone Tracker");
+         mapFrame.setSize(500, 500);
+         mapFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+         mapFrame.add(mapPanel);
+         mapFrame.pack();
+         mapFrame.setVisible(true);
+         System.out.println("map init");
 
-        //TOP IS UP DOWN
+        JPanel mainPanel = new JPanel(new BorderLayout(10, 10));
         JPanel altitudePanel = new JPanel(new FlowLayout());
         altitudePanel.add(rise);
         altitudePanel.add(lower);
 
-        //FOUR PANELS ARE EMPTY FROM HERE, YOU CAN CHANGE. THEY HAVE EMPTY TEXTS FOR CHANGING
         JPanel directionalPanel = new JPanel(new GridLayout(3, 3, 5, 5));
-
-        directionalPanel.add(picture); //EMPTY TOP LEFT
+        directionalPanel.add(picture);
         directionalPanel.add(forward);
-        directionalPanel.add(new JLabel("")); //FILL IT TOP RIGHT
-
+        directionalPanel.add(new JLabel(""));
         directionalPanel.add(left);
         directionalPanel.add(land);
-        directionalPanel.add(right);           
-
-        directionalPanel.add(new JLabel("")); // EMPTY BOTTOM LEFT
+        directionalPanel.add(right);
+        directionalPanel.add(new JLabel(""));
         directionalPanel.add(back);
-        directionalPanel.add(new JLabel("")); // FILL IT BOTTOM RIGHT
+        directionalPanel.add(new JLabel(""));
 
-        //ADDS TO THE MAIN PANEL
         mainPanel.add(altitudePanel, BorderLayout.NORTH);
         mainPanel.add(directionalPanel, BorderLayout.CENTER);
 
-        //ADDS AND VISIBLE
         frame.add(mainPanel);
         frame.setVisible(true);
 
         logger.info("start");
 
-        //TELLO CONTROLS
         TelloControl telloControl = TelloControl.getInstance();
         TelloDrone drone = TelloDrone.getInstance();
         telloControl.setLogLevel(Level.FINE);
-		camera = TelloCamera.getInstance();
+        camera = TelloCamera.getInstance();
 
-        try 
-		{
+        try {
             telloControl.connect();
             telloControl.enterCommandMode();
             telloControl.takeOff();
             telloControl.streamOn();
+            camera.startVideoCapture(true);
 
-			camera.startVideoCapture(true);  
-        } 
-		catch (Exception e) 
-		{
+            // Starts a thread in the background for socket video streaming
+            new Thread(() -> {
+                try {
+                    // Creates a socket server that listens for connections on port 9999
+                    ServerSocket serverSocket = new ServerSocket(9999);
+                    System.out.println("Waiting for client connection on port 9999...");
+
+                    // This waits until DroneVideoViewer connects
+                    Socket clientSocket = serverSocket.accept();
+                    System.out.println("Client connected!");
+
+                    // This sets up a stream to send data to DroneVideoViewer
+                    DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+
+                    // Loop that continously sends video frames
+                    while (true) {
+                        // uses OpenCV Mat to get the newest video frames
+                        Mat frameToRead = camera.getImage();
+                        if (frameToRead != null) {
+                            // This changes the Mat image into a jpg byte array
+                            MatOfByte buffer = new MatOfByte();
+                            Imgcodecs.imencode(".jpg", frameToRead, buffer);
+                            byte[] byteArray = buffer.toArray();
+
+                            //Sends length of data for the DroneVideoVIewer and sends byte array to it
+                            out.writeInt(byteArray.length);
+                            out.write(byteArray);
+                        }
+                        // has mercy on the CPU just in case
+                        Thread.sleep(100);
+                    }
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+        } catch (Exception e) {
             e.printStackTrace();
-        } 
-		finally 
-		{
+        } finally {
             if (telloControl.getConnection() == TelloConnection.CONNECTED && drone.isFlying()) {
-                try 
-				{
-                } 
-				catch (Exception e) 
-				{
+                try {
+                    // optional: cleanup
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         }
 
-        //THE VOICES LISTEN
-
-        //RISE MY GLORIOUS CREATION
+        ///RISE MY GLORIOUS CREATION
         rise.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) { telloControl.up(50); }
@@ -143,7 +175,7 @@ public class myDemo extends JFrame
         //FORWARD FOR THE HUNT
         forward.addMouseListener(new MouseAdapter() {
             @Override
-            public void mousePressed(MouseEvent e) { telloControl.forward(50); }
+            public void mousePressed(MouseEvent e) { telloControl.forward(50); mapPanel.moveUp(); }
             @Override
             public void mouseReleased(MouseEvent e) { telloControl.stop(); }
         });
@@ -151,7 +183,7 @@ public class myDemo extends JFrame
         //BACK UP THAT ASS
         back.addMouseListener(new MouseAdapter() {
             @Override
-            public void mousePressed(MouseEvent e) { telloControl.backward(50); }
+            public void mousePressed(MouseEvent e) { telloControl.backward(50); mapPanel.moveDown();}
             @Override
             public void mouseReleased(MouseEvent e) { telloControl.stop(); }
         });
@@ -159,7 +191,7 @@ public class myDemo extends JFrame
         //SWIPE LEFT UGLY BITCH
         left.addMouseListener(new MouseAdapter() {
             @Override
-            public void mousePressed(MouseEvent e) { telloControl.left(50); }
+            public void mousePressed(MouseEvent e) { telloControl.left(50); mapPanel.moveLeft();}
             @Override
             public void mouseReleased(MouseEvent e) { telloControl.stop(); }
         });
@@ -167,7 +199,7 @@ public class myDemo extends JFrame
         //SWIPE RIGHT ON MOMMY
         right.addMouseListener(new MouseAdapter() {
             @Override
-            public void mousePressed(MouseEvent e) { telloControl.right(50); }
+            public void mousePressed(MouseEvent e) { telloControl.right(50); mapPanel.moveRight(); }
             @Override
             public void mouseReleased(MouseEvent e) { telloControl.stop(); }
         });
